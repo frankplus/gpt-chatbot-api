4ffbd55b1183afd8dead0ab835c6e7583bd46d57
Francesco <pham.francesco@gmail.com>
2021-02-01 20:51:28 +0100

    pipeline

--- GePpeTto.py
+++ GePpeTto.py
@@ -1,16 +1,20 @@
-from transformers import AutoTokenizer, AutoModelWithLMHead, TextGenerationPipeline
+from transformers import AutoTokenizer, AutoModelWithLMHead, pipeline
 
 class GePpeTto:
 	def __init__(self) -> None:
 		tokenizer = AutoTokenizer.from_pretrained("./models/GePpeTto")
 		model = AutoModelWithLMHead.from_pretrained("./models/GePpeTto")
 
-		self.generator = TextGenerationPipeline(model = model, tokenizer = tokenizer)
+		self.generator = pipeline('text-generation', model=model, tokenizer=tokenizer)
 
 	def generate_text(self, prompt):
-		output = self.generator(prompt)
+		output = self.generator(
+			prompt, 
+			do_sample=True,
+			max_length=50,
+			top_k=50,
+			top_p=0.95,
+			num_return_sequences=1
+		)
 		print(output)
 		return output[0]["generated_text"][len(prompt):]
-
-generator = GePpeTto()
-print(generator.generate_text("ciao"))
